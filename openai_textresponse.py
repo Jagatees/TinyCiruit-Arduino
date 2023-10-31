e304b155af3fdda33b08c10c3f20c43478c87358
lilpineapple8 <79001107+lilpineapple8@users.noreply.github.com>
2023-10-31 20:57:16 +0800

    Add files via upload

---
+++ openai_textresponse.py
@@ -0,0 +1,18 @@
+import os
+import openai
+openai.api_key = "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"
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    #{"role": "system", "content": "The user is not able to handle spice and is vegetarian."},
+    {"role": "user", "content": "Compose 4 replies of 16 characters or less to the message {}.".format("what kind of thai dish do you want?")}
+  ]
+)
+
+choices = str(completion.choices[0].message)[39:-3]
+print(choices)
+choice = choices.split("\\n")
+print(choice)
+for i in range(0,len(choice)):
+  print(choice[i]) #send the choices in the list to arduino thru mqtt
